64b189edb250a765d46b50ef8b62d827803d1cda
hyginn <boris.steipe@utoronto.ca>
2016-09-23 17:14:37 -0400

    Provide first files

---
+++ .init.R
@@ -0,0 +1,8 @@
+# .init.R
+# Functions to initialize this Exercise session
+# Boris Steipe
+# ====================================================================
+
+file.edit("BCH441_2016.R")
+
+# [End]
---
+++ BCH441_2016.R
@@ -0,0 +1,82 @@
+# BCH441_2016.R
+#
+# Purpose:  Supporting scripts for BCH441 (Bioinformatics) at the
+#           University of Toronto, Fall 2016
+#
+# Version: 1.0
+#
+# Date:    2016  09
+# Author:  Boris Steipe (boris.steipe@utoronto.ca)
+#
+# V 1.0    First code
+#
+# TODO:
+#
+#
+# == HOW TO WORK WITH THIS FILE ======================================
+#
+#  This is the master file that will be continuously updated throughout the term
+#  to support your code development and R exercises.
+#
+#  If you wish to edit this code, save the edited version under a different
+#  name. Otherwise you will have problems with git when you update the project
+#  to a new version.
+#
+#  DO NOT simply source() this whole file!
+#
+#  If there are portions you don't understand, use R's help system,
+#  Google for an answer, or ask me. Don't continue if you don't
+#  understand what's going on. That's not how it works ...
+#
+#  While this file itself should not be edited by you ...
+#  This is YOUR file. Write your notes in here and add as many
+#  comments as you need to understand what's going on here when you
+#  come back to it in a year. That's the right way: keep code, data
+#  and notes all in one place.
+#
+# ====================================================================
+
+" Introduction:
+  ...
+"
+
+
+# ====================================================================
+#        PART ONE: REVIEW
+# ====================================================================
+
+
+
+
+# == SECTION =========================================================
+
+
+# == Subsection
+
+# Continue ...
+
+
+
+
+
+
+
+
+# ====================================================================
+#        APPENDIX: OUTLOOK
+# ====================================================================
+"There are many more functions for ... that this tutorial did not cover. You should know about the following. Look up the function and write a short bit of example code that uses it:"
+
+?subset
+?sweep
+?with     # ... and within()
+
+"Then you should know about the following packages. Open the vignette and browse through it. You should know be able to come up with least one use-case where the package functions would be useful:
+
+   https://cran.r-project.org/web/packages/magrittr/
+"
+
+
+
+
+# [END]
