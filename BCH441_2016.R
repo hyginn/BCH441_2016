11113aea1606d9a6b84730ca2288bf4e9b1aeb75
hyginn <boris.steipe@utoronto.ca>
2016-10-01 10:55:14 -0400

    First code

--- BCH441_2016.R
+++ BCH441_2016.R
@@ -1,78 +1,73 @@
 # BCH441_2016.R
 #
-# Purpose:  Supporting scripts for BCH441 (Bioinformatics) at the
-#           University of Toronto, Fall 2016
+# Purpose:  Supporting scripts for BCH441 (Bioinformatics) at the University of
+# Toronto, Fall 2016
 #
 # Version: 1.0
 #
-# Date:    2016  09
-# Author:  Boris Steipe (boris.steipe@utoronto.ca)
+# Date:    2016  09 Author:  Boris Steipe (boris.steipe@utoronto.ca)
 #
 # V 1.0    First code
 #
 # TODO:
 #
 #
-# == HOW TO WORK WITH THIS FILE ======================================
 #
-#  This is the master file that will be continuously updated throughout the term
-#  to support your code development and R exercises.
 #
-#  If you wish to edit this code, save the edited version under a different
-#  name. Otherwise you will have problems with git when you update the project
-#  to a new version.
+# ==============================================================================
+#        READ THIS FIRST
+# ==============================================================================
+
+# == HOW TO WORK WITH THE FILES IN THIS PROJECT ================================
 #
-#  DO NOT simply source() this whole file!
+# The files in this project will be continuously updated throughout the term
+# to support your code development and R exercises.
 #
-#  If there are portions you don't understand, use R's help system,
-#  Google for an answer, or ask me. Don't continue if you don't
-#  understand what's going on. That's not how it works ...
+
+# If you wish to edit any of the code, for example to add your own comments and
+# examples, save any edited version under a different name. Otherwise you will
+# have problems with git when you update the project to a new version.
+
+# DO NOT SIMPLY  source()  THIS FILE!
+
+# If there are portions you don't understand, use R's help system, Google for an
+# answer, or ask me. Don't continue if you don't understand what's going on.
+# That's not how it works ...
 #
-#  While this file itself should not be edited by you ...
-#  This is YOUR file. Write your notes in here and add as many
-#  comments as you need to understand what's going on here when you
-#  come back to it in a year. That's the right way: keep code, data
-#  and notes all in one place.
+# While this file itself should not be edited by you this is YOUR project
+# directory, and files that you create (notes etc.) will not be harmed when you
+# pull updated version of the master, or other new files, from github.
 #
-# ====================================================================
+# If you pull from github and get the following type of error ...
+#     ---------------
+#     error: Your local changes to the following files would be
+#     overwritten by merge
+#     ...
+#     Please commit your changes or stash them before you can merge.
+#     ---------------
+# ... then, you need to bring the offending file into its original state.
+# Open the Commit window, select the file, and click on the Revert button.
+#
+# Of course, you can save a local copy under a different name before you revert,
+# in case you want to keep your changes.
+#
+#
+# ==============================================================================
 
-# test 2
+# == TASK: LOCAL SCRIPT ==
+# To help keep the files of this project which are under version control intact
+# for future updates from github, use
+#     File -> New File -> R script
+# ... and create a new script for your own code experiments and notes.
+# You can name the file   "myCode.R"
+#
 
+# == TASK: THE CURRENT ASSIGNMENT ==
+# execute:
 
-# ====================================================================
-#        PART ONE: REVIEW
-# ====================================================================
+file.edit("BCH441_A03.R")
 
-
-
-
-# == SECTION =========================================================
-
-
-# == Subsection
-
-# Continue ...
-
-
-
-
-
-
-
-
-# ====================================================================
-#        APPENDIX: OUTLOOK
-# ====================================================================
-"There are many more functions for ... that this tutorial did not cover. You should know about the following. Look up the function and write a short bit of example code that uses it:"
-
-?subset
-?sweep
-?with     # ... and within()
-
-"Then you should know about the following packages. Open the vignette and browse through it. You should know be able to come up with least one use-case where the package functions would be useful:
-
-   https://cran.r-project.org/web/packages/magrittr/
-"
+# ... and close this tab.
 
 
 
