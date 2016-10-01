d8523d731efa90e3b0b3da83d6c5cc5bfbaf6c6b
hyginn <boris.steipe@utoronto.ca>
2016-10-01 10:54:24 -0400

    Code to create .myProfile.R

--- .init.R
+++ .init.R
@@ -3,6 +3,24 @@
 # Boris Steipe
 # ====================================================================
 
+# if it doesn't exist yet, set up a profile:
+if (length(list.files(pattern=".myProfile.R")) == 0) {
+    # setup profile data
+    cat("\nPlease enter the requested values correctly, no spaces, and\n")
+    cat("press <enter>.\n")
+    e <- readline("Please enter your UofT eMail address: ")
+    n <- readline("Please enter your Student Number: ")
+
+    conn <- file(".myProfile.R")
+    writeLines(c(sprintf("myEMail <- \"%s\"", e),
+                 sprintf("myStudentNumber <- %d", as.numeric(n))),
+               conn)
+    close(conn)
+    rm(e, n, conn)
+}
+
+source(".myProfile.R")
+
 file.edit("BCH441_2016.R")
 
 # [End]
